7fb5bd60501325009768dba66152020094cc28e4
falnyr <falnyr@gmail.com>
2016-03-26 19:48:58 +0100

    Langs
    Doplneni langu CS + EN

---
+++ langs/cs.php
@@ -0,0 +1,11 @@
+<?php
+
+$translations = [
+    "PUBLICATION_ADD_HTMLTITLE" => "Přidat publikaci",
+    "PUBLICATION_ADD_ISBN"      => "ISBN",
+    "PUBLICATION_ADD_TITLE"     => "Název",
+    "PUBLICATION_ADD_NUMPAGES"  => "Počet stran",
+    "PUBLICATION_ADD_EDITION"   => "Vydání",
+    "PUBLICATION_ADD_PUBLISHED" => "Datum publikace",
+    "PUBLICATION_ADD_SUBMIT"    => "Přidat",
+];
---
+++ langs/en.php
@@ -0,0 +1,11 @@
+<?php
+
+$translations = [
+    "PUBLICATION_ADD_HTMLTITLE" => "Add publication",
+    "PUBLICATION_ADD_ISBN"      => "ISBN",
+    "PUBLICATION_ADD_TITLE"     => "Title",
+    "PUBLICATION_ADD_NUMPAGES"  => "Number of pages",
+    "PUBLICATION_ADD_EDITION"   => "Edition",
+    "PUBLICATION_ADD_PUBLISHED" => "Published date",
+    "PUBLICATION_ADD_SUBMIT"    => "Add",
+];
