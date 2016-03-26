2297765960efa0dc7606baac7e642b05eb5c2d92
falnyr <falnyr@gmail.com>
2016-03-26 18:58:20 +0100

    Config
    Vytvoreni configu pro aplikaci

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * SimpleLibrary config
+ * @var array
+ */
+$cfg = [
+    // database credentials
+    "dbHost" => "localhost",
+    "dbUser" => "root",
+    "dbPass" => "",
+    "dbBase" => "simplelibrary"
+];
