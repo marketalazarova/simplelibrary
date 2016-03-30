0f102b9c3dabe6b41fe6c1bfe84e6f1cfce7de39
falnyr <falnyr@gmail.com>
2016-03-30 18:38:57 +0200

    Oprava hodnemoc successu

--- formHandler.php
+++ formHandler.php
@@ -6,7 +6,9 @@ if (isset($_POST['publication']) && count($_POST['publication'])) {
     $db = getDb();
     $query = getInsertQuery("publications", $_POST['publication']);
     if (mysqli_query($db, $query)) {
-        header("Location: ".$_SERVER["HTTP_REFERER"]."&success=1");
+        $location = $_SERVER["HTTP_REFERER"];
+        $location.= (strpos($_SERVER["HTTP_REFERER"], "success=1") === false) ? "&success=1" : "";
+        header("Location: $location");
         exit();
     }
 }
