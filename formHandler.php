78735bdf5a57658a35389db8f93d673a75541e09
Markéta Horáková Lazarová <maky.lazarova@gmail.com>
2016-03-30 23:34:45 +0200

    Funkce validace

--- formHandler.php
+++ formHandler.php
@@ -4,13 +4,48 @@ require("functions.php");
 
 if (isset($_POST['publication']) && count($_POST['publication'])) {
     $db = getDb();
+
+    validate($_POST['publication']);
+
     $query = getInsertQuery("publications", $_POST['publication']);
     if (mysqli_query($db, $query)) {
         $location = $_SERVER["HTTP_REFERER"];
         $location.= (strpos($_SERVER["HTTP_REFERER"], "success=1") === false) ? "&success=1" : "";
         header("Location: $location");
         exit();
+    } else {
+    	echo $query.PHP_EOL;
+    	echo mysqli_error($db);
     }
+}
+
+function validate(&$postParams, &$db) {
+	foreach ($postParams as $field => $value) {
+		$value = mysql_real_escape_string($db, $value);
+		switch ($field) {
+			case 'isbn':
+			case 'title':
+				if (filter_var($value, FILTER_SANITIZE_STRING) === false) {
+					$error = "$value musí byt string";
+					break 2;
+					//die("$field musi byt string");
+				}
+				break;
+			case 'edition':
+				break;
+			case 'numpages':
+				break;
+			case 'published':
+				break;
+			
+			default:
+				break;
+		}
+	}
+
+	if(isset($error)) {
+		die($error);
+	}
 }
 
 echo "error occurred";
