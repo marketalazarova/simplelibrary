85d2f94d390095863511ba61ab3b8f508169dc3a
Markéta Horáková Lazarová <maky.lazarova@gmail.com>
2016-04-05 11:51:17 +0200

    Nastaven výchozí jazyk
    Výchozí jazyk pro aplikaci nastaven na češtinu.

--- config.php
+++ config.php
@@ -10,3 +10,10 @@ $cfg = [
     "dbPass" => "",
     "dbBase" => "simplelibrary"
 ];
+
+
+//Set default language if it is not set in URL parametr 'lang'
+
+if ((isset($_GET['lang'])) === FALSE) {
+	$_GET['lang'] = 'cs';
+}
